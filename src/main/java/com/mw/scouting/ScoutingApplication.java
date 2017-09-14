ff2b101a40cc3564e33f98f2f2f5a282ec6696f9
mwojj <maciek.wojewodzki94@gmail.com>
2017-09-14 23:24:56 +0200

    first commit

---
+++ src/main/java/com/mw/scouting/ScoutingApplication.java
@@ -0,0 +1,13 @@
+package com.mw.scouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ScoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mw/scouting/ScoutingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mw.scouting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoutingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
